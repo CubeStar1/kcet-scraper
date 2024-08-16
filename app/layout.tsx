d0a9ad581aef7add57daf05d71a11db929dacaac
CubeStar1 <avinash.anish04@gmail.com>
2024-08-16 20:50:36 +0530

    Added clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 import NextTopLoader from 'nextjs-toploader';
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +37,15 @@ export default function RootLayout({
            />
           {children}
           <Analytics />
+          <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
         </ThemeProvider>
       </body>
     </html>
