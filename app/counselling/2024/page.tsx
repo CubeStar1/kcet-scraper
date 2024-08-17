c161720a1edad1c8ef4b434fa4588c6f89e98105
CubeStar1 <avinash.anish04@gmail.com>
2024-08-18 00:31:21 +0530

    Added cached requests

--- app/counselling/2024/page.tsx
+++ app/counselling/2024/page.tsx
@@ -2,7 +2,7 @@
 import PaginatedTable from '@/components/PaginatedTable'
 
 async function getInitialData(page: number, pageSize: number) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data/2024?page=${page}&pageSize=${pageSize}`, { cache:  'no-store' })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data/2024?page=${page}&pageSize=${pageSize}`)
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
