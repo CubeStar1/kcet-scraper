22c1c174e713615614aaa27a8ab5cdc4d50d8051
CubeStar1 <avinash.anish04@gmail.com>
2024-08-26 01:05:13 +0530

    Bug fix

--- app/cutoffs/page.tsx
+++ app/cutoffs/page.tsx
@@ -65,7 +65,7 @@ export default function Component() {
             onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
           >
              {`${category} Cutoff`}
-            <ArrowUpDown className="ml-2 h-4 w-4" />
+            <ArrowUpDown className="ml-2 h-4 w-4 " />
           </Button>
         )
       }
