c8b028de37dc042112f9ad3f9efe848d16ae46fa
CubeStar1 <avinash.anish04@gmail.com>
2024-08-26 14:36:38 +0530

    Wrapped useSearchParams in Suspense boundary

--- app/counselling/2023/page.tsx
+++ app/counselling/2023/page.tsx
@@ -1,12 +1,14 @@
 import PaginatedTable from '@/components/PaginatedTable'
-
+import { Suspense } from 'react'
 export default function Home() {
   return (
     <main className="container mx-auto p-4 max-h-screen">
       <h1 className="text-2xl font-bold mb-4" >
         Candidate Data: 2023 
       </h1>
-      <PaginatedTable year="2023" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PaginatedTable year="2023" />
+      </Suspense>
     </main>
   )
 }
--- app/counselling/2024/page.tsx
+++ app/counselling/2024/page.tsx
@@ -1,6 +1,7 @@
 import PaginatedTable from '@/components/PaginatedTable'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { Info } from 'lucide-react'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
@@ -14,8 +15,9 @@ export default function Home() {
           Note: The data for mock round 2 is still being scraped, this is the data for mock round 1
         </CardContent>
       </Card>
-
-      <PaginatedTable year="2024" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PaginatedTable year="2024" />
+      </Suspense>
     </main>
   )
 }
