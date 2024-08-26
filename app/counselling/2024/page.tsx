4cd4d77f89afe7b94ff86a200cd0ad9a492cb20c
CubeStar1 <avinash.anish04@gmail.com>
2024-08-26 09:38:56 +0530

    Updated Note

--- app/counselling/2024/page.tsx
+++ app/counselling/2024/page.tsx
@@ -1,5 +1,7 @@
 // // app/page.tsx
 import PaginatedTable from '@/components/PaginatedTable'
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import { Info } from 'lucide-react'
 
 async function getInitialData(page: number, pageSize: number) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data/2024?page=${page}&pageSize=${pageSize}`)
@@ -20,6 +22,13 @@ export default async function Home({ searchParams }: { searchParams: { page: str
       <h1 className="text-2xl font-bold mb-4" >
         Candidate Data: 2024
       </h1>
+      <Card>
+          <CardContent className='flex items-center mt-4'>
+            <Info className="w-4 h-4 mr-2" />
+            Note: The data for mock round 2 is still being scraped, this is the data for mock round 1
+          </CardContent>
+      </Card>
+
       <PaginatedTable initialData={data} initialTotalCount={count} year="2024" />
     </main>
   )
